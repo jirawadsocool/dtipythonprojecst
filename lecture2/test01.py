28c836ecf499351f2b8b52115ae5c27b3b92a2ec
jirawadsocool <s6652410033@sau.ac.th>
2023-08-22 09:48:28 +0700

    code V.1: create 4 file eiei jubjub......

---
+++ lecture2/test01.py
@@ -0,0 +1,17 @@
+print('Hello,\
+World.')
+print('Hi,World.')
+print('Hey,World.')
+print("Hum,World.")
+print("123456")
+
+print('''
+      DTI
+      ^o^
+      SAU
+      ''')
+
+print("""
+      ID : 6XXXXXXXXXXXXXXXXX
+      Nama : Jirawad
+      """)
